f714bbaa8e661ffea6335075edacb083cea99671
jaeung.choi <jaeung.choi@navercorp.com>
2021-11-03 22:47:54 +0900

    Week 1 - 13. Finishing touches!

--- week 1-Jetpack Compose basics/BasicsComposeCodelab/app/src/main/java/com/example/codelabproject/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicsComposeCodelab/app/src/main/java/com/example/codelabproject/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.codelabproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
@@ -10,6 +11,9 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -18,6 +22,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,7 +41,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun MainApp() {
-    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(false) }
 
     if (shouldShowOnboarding) {
         OnboardingScreen(onContinueClick = { shouldShowOnboarding = false })
@@ -57,22 +62,23 @@ fun Greetings(names: List<String> = List(1000) { "$it" }) {
 @Composable
 fun Greeting(name: String) {
     var isExpanded by remember { mutableStateOf(false) }
-    val extraPadding by animateDpAsState(
-        targetValue = if (isExpanded) 48.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        )
-    )
+
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 4.dp)
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
+        Row(modifier = Modifier.padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+                    .padding(bottom = 12.dp)
+
             ) {
                 Text(text = "Hello")
                 Text(
@@ -81,11 +87,18 @@ fun Greeting(name: String) {
                         fontWeight = FontWeight.ExtraBold
                     )
                 )
+                if(isExpanded) {
+                    Text(text = "blah blah".repeat(30))
+                }
             }
-            OutlinedButton(onClick = {
-                isExpanded = !isExpanded
-            }) {
-                Text(text = if (isExpanded) "show less" else "show more")
+            IconButton(onClick = { isExpanded = !isExpanded }) {
+                Icon(
+                    imageVector = if (isExpanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription = when (isExpanded) {
+                        true -> stringResource(R.string.show_more)
+                        false -> stringResource(R.string.show_less)
+                    }
+                )
             }
         }
     }
