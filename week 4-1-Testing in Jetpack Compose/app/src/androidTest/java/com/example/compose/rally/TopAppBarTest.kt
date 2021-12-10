bb242fafc5464bd504decc56b9297baacd986345
Jaeung Choi <darker826@gmail.com>
2021-12-10 21:20:25 +0900

    compose codelab 11-6~7

--- week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -1,6 +1,7 @@
 package com.example.compose.rally
 
 import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.junit4.createComposeRule
 import com.example.compose.rally.ui.components.RallyTopAppBar
 import org.junit.Rule
@@ -9,7 +10,7 @@ import java.util.*
 
 class TopAppBarTest {
     @get:Rule
-    val composeTestRule = createComposeRule()
+    val composeTestRule = createAndroidComposeRule(RallyActivity::class.java)
 
     @Test
     fun rallyTopAppBarTest() {
@@ -62,5 +63,27 @@ class TopAppBarTest {
                 useUnmergedTree = true
             )
             .assertExists()
+    }
+
+    @Test
+    fun rallyTopAppBarTest_tabClick() {
+//        val allScreens = RallyScreen.values().toList()
+//        androidComposeTestRule.setContent {
+//            RallyTopAppBar(
+//                allScreens = allScreens,
+//                onTabSelected = { },
+//                currentScreen = RallyScreen.Overview
+//            )
+//        }
+
+        composeTestRule.onRoot().printToLog("tabClick")
+
+        composeTestRule
+            .onNodeWithContentDescription(
+                RallyScreen.Accounts.name,
+                useUnmergedTree = true
+            ).assertIsNotSelected()
+            .performClick()
+            .assertIsSelected()
     }
 }
